24e52a092a6ac3a770961b3a97ee90156f739c5b
hgobr <hugo.brothier@hotmail.fr>
2022-02-22 21:49:41 +0100

    update App.js

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { setStatusBarStyle, StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Vibration,
+  Linking,
+} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('React Native App');
+
+  const onClick = () => {
+    setName('Dev mobile app with Hugo');
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="inverted" />
+      <Text style={styles.Text}>{name}</Text>
+      <Button
+        color="red"
+        title="Update"
+        onPress={() => {
+          Vibration.vibrate();
+          onClick();
+        }}
+      ></Button>
     </View>
   );
 }
@@ -13,8 +34,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0e141b',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#fff',
+  },
+  Text: {
+    color: '#fff',
+    fontSize: 25,
+    margin: 10,
   },
 });
