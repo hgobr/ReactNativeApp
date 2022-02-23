a47afa0c9ec6180eed5775c800d882c5f94af046
hgobr <hugo.brothier@hotmail.fr>
2022-02-24 00:18:43 +0100

    update main files

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
   Linking,
   Pressable,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 
 export default function App() {
@@ -24,11 +25,26 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="inverted" />
-      <Text style={styles.Text}>{value}</Text>
-      <TouchableOpacity style={styles.button} onPress={() => onClick()}>
-        <Text style={styles.Text}>ADD</Text>
-      </TouchableOpacity>
-      <Text style={styles.Text}>You hit the button {hitButton} times</Text>
+      <View style={styles.wrapButton}>
+        <Text style={styles.Text}>{value}</Text>
+        <TouchableOpacity style={styles.button} onPress={() => onClick()}>
+          <Text style={styles.Text}>ADD 5</Text>
+        </TouchableOpacity>
+        <Text style={styles.Text}>You hit the button {hitButton} times</Text>
+      </View>
+      <View style={styles.wrapSquares}>
+        <ScrollView>
+          <View style={[styles.viewBase, styles.view01]}>
+            <Text>1</Text>
+          </View>
+          <View style={[styles.viewBase, styles.view02]}>
+            <Text>2</Text>
+          </View>
+          <View style={[styles.viewBase, styles.view03]}>
+            <Text>3</Text>
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -46,9 +62,34 @@ const styles = StyleSheet.create({
     fontSize: 25,
     margin: 10,
   },
+  wrapButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
   button: {
     borderRadius: 5,
     backgroundColor: '#539BF3',
     margin: 5,
+  },
+  wrapSquares: {
+    flexDirection: 'row',
+  },
+  viewBase: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 5,
+    borderRadius: 5,
+  },
+  view01: {
+    backgroundColor: '#99EDCC',
+  },
+  view02: {
+    backgroundColor: '#CB958E',
+  },
+  view03: {
+    backgroundColor: '#E36588',
   },
 });
